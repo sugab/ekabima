5bbac40371d7c2732062256d46b2a44af7d19760
Bagus Cahyono <baguscah77@gmail.com>
2016-06-02 14:40:55 +0700

    adding permit params in AA order

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register Order do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :user_id, :name, :phone, :school_name, :school_phone, :school_address, :province, :city, :school_domain_name, :requirement_file_path, :status
 #
 # or
 #
