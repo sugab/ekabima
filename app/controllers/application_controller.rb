d0396581ac82790cade17157a8c78e79dcd1a010
Bagus Cahyono <baguscah77@gmail.com>
2016-06-05 20:36:14 +0700

    Setting session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    dashboards_index_path
+    if resource.instance_of? AdminUser
+      admin_root_path
+    else
+      dashboard_path
+    end
   end
 
   def after_sign_out_path_for(resource)
